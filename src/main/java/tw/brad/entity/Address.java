32bcdc9c75f4dec95e7dd68485f95bd2454f668d
mickeyyo55b <mickeyyo55b@gmail.com>
2025-07-23 15:04:46 +0800

    07231504

---
+++ src/main/java/tw/brad/entity/Address.java
@@ -0,0 +1,28 @@
+package tw.brad.entity;
+
+public class Address {
+	private String city;
+	private String town;
+	private String addr;
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getTown() {
+		return town;
+	}
+	public void setTown(String town) {
+		this.town = town;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	
+	
+}
---
+++ src/main/java/tw/brad/entity/User.java
@@ -0,0 +1,38 @@
+package tw.brad.entity;
+
+import java.util.List;
+
+public class User {
+	private String name;
+	private int age;
+	private Address address;
+	private List<Phone> phones;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public List<Phone> getPhones() {
+		return phones;
+	}
+	public void setPhones(List<Phone> phones) {
+		this.phones = phones;
+	}
+	
+	
+}
