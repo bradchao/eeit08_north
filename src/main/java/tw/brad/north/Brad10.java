b5e161335bc6e93697c09985ff66882affd4ce76
mickeyyo55b <mickeyyo55b@gmail.com>
2025-07-23 15:40:55 +0800

    07231540

---
+++ src/main/java/tw/brad/north/Brad10.java
@@ -0,0 +1,44 @@
+package tw.brad.north;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Brad10 {
+
+	public static void main(String[] args) {
+		String json = """
+					{
+						"name" : "Brad",
+						"age": 18,
+						"address": {
+							"city": "台中市",
+							"town": "南屯區",
+							"addr": "公益路100號"
+						},
+						"phones": [
+							{
+								"type": "CHT",
+								"number": "0912-123456"
+							},
+							{
+								"type": "ABC",
+								"number": "0912-765432"
+							}
+						]
+					}				
+				""";
+		ObjectMapper mapper = new ObjectMapper();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
